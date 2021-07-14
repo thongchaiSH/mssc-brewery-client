f82a4bcf845632016fa7cd4073565368d1c3947e
thongchai <thongchai.sh@gmail.com>
2021-07-14 23:07:31 +0700

    Add Integration test BreweryClient. closes #2

---
+++ src/test/java/com/ith/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package com.ith.msscbreweryclient.web.client;
+
+import com.ith.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto=client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+}
